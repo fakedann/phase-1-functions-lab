4de5631f2fbc086d685af936ffd302a573e10f91
Daniel Escalona <daniel07escalona@gmail.com>
2022-01-11 12:23:55 -0500

    commit tree f6b582f98ec5a5c4fe93806ccb6f737c40090982

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(42-someValue);
+}
+
+function distanceFromHqInFeet(value2) {
+    return 264*distanceFromHqInBlocks(value2);
+}
+
+function distanceTravelledInFeet(pos1, pos2){
+    return Math.abs(pos1-pos2)*264;
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+    
+    if (feet < 401){
+        return 0;
+    }else if (feet < 2001){
+        return ((feet-400)*2)/100;
+    }else if (feet < 2501){
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+}
